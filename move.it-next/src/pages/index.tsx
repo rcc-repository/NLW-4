e4a2ab715b8d83da489934ed0fb4c7513d911179
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-02-25 18:29:50 -0300

    Codando

--- move.it-next/src/pages/index.tsx
+++ move.it-next/src/pages/index.tsx
@@ -1,7 +1,10 @@
 
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
 
@@ -9,12 +12,17 @@ export default function Home() {
   return (
 
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.It</title>
+      </Head>
+      
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div>
