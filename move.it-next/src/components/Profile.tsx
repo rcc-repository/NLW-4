444ed9f22de7adfd9f6017208e6d5bfff1fc0360
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-02-25 17:39:13 -0300

    Codando

--- move.it-next/src/components/Profile.tsx
+++ move.it-next/src/components/Profile.tsx
@@ -9,7 +9,11 @@ export function Profile () {
 
             <div>
                 <strong> Rodrigo Costa </strong>
-                <p>Level 1</p>
+                
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
             </div>
         </div>
     )
--- move.it-next/src/pages/index.tsx
+++ move.it-next/src/pages/index.tsx
@@ -1,4 +1,5 @@
 
+import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 
@@ -7,12 +8,13 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
 
-    <div className={styles.contianer}>
+    <div className={styles.container}>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
 
         <div>
