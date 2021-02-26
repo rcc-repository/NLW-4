54413c2af4e76b8b7f75b0248aa0ac1f5cccb256
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-02-26 11:31:34 -0300

    Codando

--- move.it-next/src/pages/index.tsx
+++ move.it-next/src/pages/index.tsx
@@ -1,12 +1,13 @@
+import Head from 'next/head';
 
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
-
-import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
         </div>
 
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
