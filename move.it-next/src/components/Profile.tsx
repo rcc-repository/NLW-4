b8ed94045f73fe8b8290e53e6b811360eae27fb1
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-03-01 10:52:19 -0300

    Codando

--- move.it-next/src/components/Profile.tsx
+++ move.it-next/src/components/Profile.tsx
@@ -7,9 +7,9 @@ export function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/diego3g.png" alt="Diego Fernandes "/>
+      <img src="https://github.com/rcc-repository.png" alt="Rodrigo Costa"/>
       <div>
-        <strong>Diego Fernandes</strong>
+        <strong>Rodrigo Costa</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Level {level}
